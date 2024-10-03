2a6e68359da05d9a2802b95b23fd9573612f4de2
Nathan Samuell <143547836+nathansamuell@users.noreply.github.com>
2024-10-03 13:27:53 -0500

    Create error_handling.py

---
+++ error_handling.py
@@ -0,0 +1,30 @@
+print("Script started!")
+
+def divideNums(num1, num2):
+  result = num1 / num2;
+  print(result)
+
+# This code works normally
+divideNums(7, 9)
+divideNums(9, 32)
+
+# this code causes an error:
+divideNums(8, 0)  # here, the program ends!!
+
+# how can we handle this unexpected behavior?
+
+def divideNumsWithErrorHandled(num1, num2):
+  try:
+    result = num1 / num2
+    print(result)
+  except ZeroDivisionError:
+    print("Error: cannot divide by zero")
+    print("Program keeps running!!")
+  except Error as e:
+    print(str(e))
+
+
+divideNumsWithErrorHandled(8, 0)
+
+
+
